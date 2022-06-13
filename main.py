8e9c30bb7cd6fc49c9005dce418a614363bee80f
Edikscrmp <76255209+Edikscrmp@users.noreply.github.com>
2022-06-13 17:43:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+import random
+bot = telebot.TeleBot("5575414666:AAGTvlmE6vcuSFuQvuEMkYdiuZiN-0JmupY")
+
+list=["Круто!", "Превосходно!","Восхитительно!","Хайпово","Супер!","Прелестно!"]
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if "Цена на товар снижена:" in message.text:
+        bot.send_message(message.chat.id, "Ого")
+        bot.send_message(message.chat.id, list[random.randint(0, 5)])
+
+bot.polling(none_stop=True, interval=0)
